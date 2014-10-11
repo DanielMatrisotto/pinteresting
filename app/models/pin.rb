f33ebee65feecaea31a90223d64a5274cc196ceb
DanielMatrisotto <daniel.matrisotto@gmail.com>
2014-10-11 11:46:11 +0200

    added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	do_not_validate_attachment_file_type :image
+	# Neue Pins anlegen geht nicht ohne Bild und Beschreibung
+	validates :image, presence: true
+	validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
+
+  # Man muss den Namen angeben bei Neuanmelden oder Ändern von Dateien
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
